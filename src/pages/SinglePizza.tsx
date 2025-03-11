cc2ad11912682a7a0228d9df6a432ad1ebfa8e3e
BorosBalazsLaszlo <c9borbal@vasvari.org>
2025-03-11 14:50:36 +0100

    megtekintes kesz

--- src/pages/SinglePizza.tsx
+++ src/pages/SinglePizza.tsx
@@ -4,7 +4,7 @@ import { Pizza } from '../types/Pizza';
 import apiClient from '../api/api';
 import toastFailed from '../toasts/toastFailed';
 import toastSuccess from '../toasts/toastSuccess';
-import '../css/singlepizza.css'
+import '../css/singlepizza.css';
 
 function SinglePizza() {
     const { id } = useParams();
@@ -37,9 +37,9 @@ function SinglePizza() {
         fetchPizza();
     }, [id]);
 
-    const updatePizza = async () =>{
-        try{
-            const updatedPizza ={
+    const updatePizza = async () => {
+        try {
+            const updatedPizza = {
                 ar: ar,
                 imageUrl: imageUrl,
                 leiras: leiras,
@@ -47,40 +47,46 @@ function SinglePizza() {
             } as Pizza;
 
             const response = await apiClient.put(`/pizzak/${id}`, updatedPizza);
-            toastSuccess("Sikeres frissítés!");
-        } catch (err:any)
-        {
-            toastFailed("Sikertelen frissítés!");
+            toastSuccess('Sikeres frissítés!');
+        } catch (err: any) {
+            toastFailed('Sikertelen frissítés!');
         }
     };
- 
+
     return (
         <div>
-            <h1>Név{pizza?.nev}</h1>
+            <div className="information">
+                <h2>Név{pizza?.nev}</h2>
+                <p>Ár{pizza?.ar}</p>
+                <p>Leírás: sdasdasdasdasdadasdasdasdasdasdsadsad{pizza?.leiras}</p>
+            </div>
+            <img className="floating" alt={pizza?.nev} src={`https://placehold.co/600x400`} />
 
-            <p>Ár{pizza?.ar}</p>
-            <p>Leírás: sdasdasdasdasdadasdasdasdasdasdsadsad{pizza?.leiras}</p>
-
-            <img className='floating' alt={pizza?.nev} src={`https://placehold.co/600x400`} />
-        
-
-            <div className='main-put'>
+            <div className="main-put">
                 <h4>Adatok frissítése</h4>
                 <form action="submit">
-                <input type="text" placeholder='Név' onChange={(e) => setNev(e.target.value)}/>
-                <div>
-                <input type="number" placeholder='Ár (Ft)' onChange={(e) => setAr(Number(e.target.value))}/>
-                </div>
-                <input type="text" placeholder='Kép url' onChange={(e) => setImageUrl(e.target.value)}/>
-                <div className="leiras">
-                    <textarea
-                        placeholder="Leírás"
-                        onChange={(e) => setLeiras(e.target.value)}
+                    <input type="text" placeholder="Név" onChange={(e) => setNev(e.target.value)} />
+                    <div>
+                        <input
+                            type="number"
+                            placeholder="Ár (Ft)"
+                            onChange={(e) => setAr(Number(e.target.value))}
+                        />
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Kép url"
+                        onChange={(e) => setImageUrl(e.target.value)}
                     />
-                </div>
+                    <div className="leiras">
+                        <textarea
+                            placeholder="Leírás"
+                            onChange={(e) => setLeiras(e.target.value)}
+                        />
+                    </div>
                 </form>
 
-                <button type='submit' onClick={updatePizza}>
+                <button type="submit" onClick={updatePizza}>
                     Frissítés
                 </button>
             </div>
